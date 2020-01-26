2a16b490a2ffc7c1a10613f14c3cb1e3bdb0e55c
Marcos Couto <marcos_otuoc@hotmail.com>
2020-01-26 20:50:59 -0300

    First commit

---
+++ src/main/java/com/marcoscouto/webservicesjpahibernate/WebservicesJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.webservicesjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcoscouto/webservicesjpahibernate/WebservicesJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.webservicesjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
