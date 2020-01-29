cd68d057f06690e51b6b1ad2e4a81edc238ec1bd
Marcos Couto <marcos_otuoc@hotmail.com>
2020-01-29 16:07:43 -0300

    User insert

--- src/main/java/com/marcoscouto/webservicesjpahibernate/resources/UserResource.java
+++ src/main/java/com/marcoscouto/webservicesjpahibernate/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.marcoscouto.webservicesjpahibernate.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +27,16 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
